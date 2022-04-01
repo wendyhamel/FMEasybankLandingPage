67543018696e9f09d03031d5e6761259d8513f9c
wendyhamel <whamel@it4web.nl>
2022-04-01 17:02:48 +0200

    Layout page, started styling header

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config  = {
+    theme: {
+        fontFamily: {
+            'sans': ['Public\\ Sans', 'sans-serif'],
+        },
+        colors: {
+            'dark-blue': 'hsl(233, 26%, 24%)',
+            'lime-green': 'hsl(136, 65%, 51%)',
+            'bright-cyan': 'hsl(192, 70%, 51%)',
+            'grayish-blue': 'hsl(233, 8%, 62%)',
+            'light-grayish-blue': 'hsl(220, 16%, 96%)',
+            'very-light-gray': 'hsl(0, 0%, 98%)',
+            'white': 'hsl(0, 0%, 100%)',
+        },
+    },
+}
