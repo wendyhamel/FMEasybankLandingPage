9dba5f6ddb0806f7f013f31ef3c65abc384f139f
wendyhamel <whamel@it4web.nl>
2022-04-04 11:47:30 +0200

    Mobile styling

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -7,10 +7,26 @@ tailwind.config  = {
             'dark-blue': 'hsl(233, 26%, 24%)',
             'lime-green': 'hsl(136, 65%, 51%)',
             'bright-cyan': 'hsl(192, 70%, 51%)',
-            'grayish-blue': 'hsl(233, 8%, 62%)',
-            'light-grayish-blue': 'hsl(220, 16%, 96%)',
+            'blue-gray': 'hsl(233, 8%, 62%)',
+            'light-blue-gray': 'hsl(220, 16%, 96%)',
             'very-light-gray': 'hsl(0, 0%, 98%)',
             'white': 'hsl(0, 0%, 100%)',
         },
+        extend: {
+            spacing: {
+                '4.5': '1.125rem',
+                '22': '5.5rem'
+            },
+            fontSize: {
+                'xxs': '0.65rem',
+                '4.5xl': ['2.5rem', {
+                    lineHeight: '46px'
+                }],
+            },
+            lineHeight: {
+                '4.5': '1.125rem',
+            }
+        }
+
     },
 }
